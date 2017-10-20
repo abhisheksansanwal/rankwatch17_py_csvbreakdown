2a6796cf77b50136fc729e6389862b6baf6ae8c1
abhisheksansanwal <30996223+abhisheksansanwal@users.noreply.github.com>
2017-10-19 20:38:41 -0700

    Add files via upload

---
+++ observe raw directory.py
@@ -0,0 +1,10 @@
+#To work on  data analysing us package pandas
+import panda as pd
+#To read the csv file 
+pd.read_csv('Testdata.csv'
+#The data is merging
+frame1=pd.Dataframe ({key:range(5)})
+frame1=pd.Data frame ({key:range(5)})'frame1':['a','b','c','d','e']})
+frame2=pd.Data frame ({key:range(5)})'frame2':['t','u','v','w','x']})
+pd.merge(frame1,frame2,on='key',how='left')
+
